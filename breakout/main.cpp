10e80a028567bc44e158db4c55a0cf486d694b6f
Yuvaraj Chesetti <karch08@gmail.com>
2017-09-24 01:56:03 +0530

    Refactor a bit..

--- breakout/main.cpp
+++ breakout/main.cpp
@@ -3,46 +3,57 @@
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
+const char* SCREEN_NAME = "Breakout!";
 
+// TODO(chesetti): What's the best way to handle error checking?
 
-int main(int argc, char** argv) {
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
 
-
-	// All SDL calls seem to start as SDL_*
-
-	// Step 1. Initialize SDL and the subsystems you want to use.
-	// TODO(chesetti): What's the best way to handle error checking?
+bool initialize() {
+	// Initialize SDL and the subsystems you want to use.
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		SDL_Log("Unable to initialize SDL: %s\n", SDL_GetError());
-		return 1;
+		return false;
 	}
 
 	// Initialize the Window.
-	SDL_Window* window = NULL;
-	window = SDL_CreateWindow(
-		"Breakout",
+	gWindow = SDL_CreateWindow(
+		SCREEN_NAME,
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
 		SCREEN_WIDTH,
 		SCREEN_HEIGHT,
 		SDL_WINDOW_SHOWN
 	);
-
-	if (window == NULL) {
+	if (gWindow == NULL) {
 		SDL_Log("Unable to create window: %s\n", SDL_GetError());
+		return false;
+	}
+
+	gScreenSurface = NULL;
+	gScreenSurface = SDL_GetWindowSurface(gWindow);
+
+	return true;
+}
+
+void close() {
+	// LazyFoo says this also takes care of Window Surface.
+	SDL_DestroyWindow(gWindow);
+	SDL_Quit();
+}
+
+int main(int argc, char** argv) {
+
+
+	if (!initialize()) {
 		return 1;
 	}
 
-	// Initialize the Surface. A surface represents a bunch of pixels?
-	// Surface is something you draw onto.
-	SDL_Surface* screenSurface = NULL;
-	screenSurface = SDL_GetWindowSurface(window);
-
-	// Anything special about this?
+	// All the action goes here.
 	SDL_Delay(2000);
 
-	// LazyFoo says this also takes care of the Surface.
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+	close();
+
 	return 0;
 }
