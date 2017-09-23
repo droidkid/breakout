f6e8fb9bf67ea239e034ea3de493b6d302e2d12f
Yuvaraj Chesetti <karch08@gmail.com>
2017-09-24 01:19:04 +0530

    More comments.

--- breakout/main.cpp
+++ breakout/main.cpp
@@ -12,19 +12,25 @@ int main(int argc, char** argv) {
 
 	// Step 1. Initialize SDL and the subsystems you want to use.
 	// TODO(chesetti): What's the best way to handle error checking?
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		printf("SDL could not initialize: %s\n", SDL_GetError());
-		return 0;
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		SDL_Log("Unable to initialize SDL: %s\n", SDL_GetError());
+		return 1;
 	}
 
 	// Initialize the Window.
 	SDL_Window* window = NULL;
-	window = SDL_CreateWindow("Breakout", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	window = SDL_CreateWindow(
+		"Breakout",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		SCREEN_WIDTH,
+		SCREEN_HEIGHT,
+		SDL_WINDOW_SHOWN
+	);
 
-	// TODO(chesetti): What's the best way to handle error checking?
 	if (window == NULL) {
-		printf("Window could not be initialized: %s\n", SDL_GetError());
-		return 0;
+		SDL_Log("Unable to create window: %s\n", SDL_GetError());
+		return 1;
 	}
 
 	// Initialize the Surface. A surface represents a bunch of pixels?
@@ -35,8 +41,7 @@ int main(int argc, char** argv) {
 	// Anything special about this?
 	SDL_Delay(2000);
 
-	// TODO(chesetti): Why is this needed? SDL_Quit seems to work just fine?
-	// How do I just use the window buttons?
+	// LazyFoo says this also takes care of the Surface.
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
