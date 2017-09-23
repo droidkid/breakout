fddab60c7f9a2d73d87c0abb254cb94e4b53764b
Yuvaraj Chesetti <karch08@gmail.com>
2017-09-24 00:28:58 +0530

    Initial Commit. Get's a window up and running.

--- breakout/main.cpp
+++ breakout/main.cpp
@@ -10,12 +10,14 @@ int main(int argc, char** argv) {
 
 	// All SDL calls seem to start as SDL_*
 
+	// Step 1. Initialize SDL and the subsystems you want to use.
 	// TODO(chesetti): What's the best way to handle error checking?
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		printf("SDL could not initialize: %s\n", SDL_GetError());
 		return 0;
 	}
 
+	// Initialize the Window.
 	SDL_Window* window = NULL;
 	window = SDL_CreateWindow("Breakout", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
@@ -25,13 +27,13 @@ int main(int argc, char** argv) {
 		return 0;
 	}
 
+	// Initialize the Surface. A surface represents a bunch of pixels?
 	// Surface is something you draw onto.
 	SDL_Surface* screenSurface = NULL;
 	screenSurface = SDL_GetWindowSurface(window);
 
 	// Anything special about this?
 	SDL_Delay(2000);
-
 
 	// TODO(chesetti): Why is this needed? SDL_Quit seems to work just fine?
 	// How do I just use the window buttons?
