7450a73b51c0065f3d793a8ab0d7b8de00718b29
Yuvaraj Chesetti <karch08@gmail.com>
2017-11-26 20:34:55 +0530

    Change order of Breakout.

--- breakout/breakout.h
+++ breakout/breakout.h
@@ -5,7 +5,7 @@
 #include "paddle.h"
 #include "brick.h"
 #include "resources.h"
-#include "sdl_helper.h"
+#include "sdl_system.h"
 #include "collision_engine.h"
 #include "screen.h"
 #include "event_queue.h"
@@ -19,7 +19,7 @@ class Breakout : public Screen {
 	EventQueue *events;
 
 public:
-	Breakout(Resources *resources, SDLHelper *sdlComponent, EventQueue *events);
+	Breakout(SDLSystem *sdl, Resources *resource, EventQueue *events);
 	void update();
 	void draw();
 	~Breakout();
